fe2c9758d7a8d9ce1b4ec3685c796381fb82ef96
nedois <natael.bernardo@hotmail.com>
2022-10-12 00:50:39 +0200

    test sign up use case exception code

--- libs/user-account/use-cases/src/sign-up/__tests__/sign-up-use-case.spec.ts
+++ libs/user-account/use-cases/src/sign-up/__tests__/sign-up-use-case.spec.ts
@@ -3,6 +3,7 @@ import {
   UserMailerMock,
   UserAlreadyExistsException,
 } from '@moona-backend/user-account/domain';
+import { ENTITY_EXCEPTION_CODE } from '@moona-backend/common/domain';
 import { SignUpUseCasePayload } from '../sign-up.interfaces';
 import { SignUpUseCase } from '../sign-up.use-case';
 
@@ -33,17 +34,20 @@ describe('Sign Up use case', () => {
   });
 
   it('should throw "Entity Already Exists" error if email is already taken', async () => {
-    const payload: SignUpUseCasePayload = {
-      email: 'john.doe@example.com',
-      firstName: 'John',
-      lastName: 'Doe',
-      password: '123456',
-    };
-
     jest.spyOn(userRepository, 'countByEmail').mockResolvedValue(1);
 
     const useCase = new SignUpUseCase(userRepository, userMailer);
 
-    await expect(useCase.execute(payload)).rejects.toThrow(UserAlreadyExistsException);
+    try {
+      await useCase.execute({
+        email: 'john.doe@example.com',
+        firstName: 'John',
+        lastName: 'Doe',
+        password: '123456',
+      });
+    } catch (e) {
+      expect(e).toBeInstanceOf(UserAlreadyExistsException);
+      expect(e.code).toBe(ENTITY_EXCEPTION_CODE.ALREADY_EXISTS);
+    }
   });
 });
