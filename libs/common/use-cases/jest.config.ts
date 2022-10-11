6a34884159fb2200fb8883ad36d968202cf7d8f7
nedois <natael.bernardo@hotmail.com>
2022-10-11 23:10:13 +0200

    change application lib name to use-cases

--- libs/common/use-cases/jest.config.ts
+++ libs/common/use-cases/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'common-application',
+  displayName: 'common-use-cases',
   preset: '../../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/libs/common/application',
+  coverageDirectory: '../../../coverage/libs/common/use-cases',
 };
