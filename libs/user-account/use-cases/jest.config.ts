87fde47def2443019dabdcf9e894c484c273299b
nedois <natael.bernardo@hotmail.com>
2022-10-11 23:08:52 +0200

    create user account use-cases lib

---
+++ libs/user-account/use-cases/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'user-account-use-cases',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/libs/user-account/use-cases',
+};
